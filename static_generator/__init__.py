4254bdac10102b79425a074b7e59b221ea64b90f
incora-dev <dev@incorainc.com>
2018-11-27 18:25:27 +0200

    fixed losted arguments

--- static_generator/__init__.py
+++ static_generator/__init__.py
@@ -4,5 +4,5 @@ def login_required(f):
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated:
             return render(request, 'default_pages/auth_required.html', {})
-        return f(request)
+        return f(request, *args, **kwargs)
     return wrapper
